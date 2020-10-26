17e4c1ad7497f98675e486156a304f1478c3c9f3
zelioh <38160465+zelioh@users.noreply.github.com>
2020-10-26 11:16:38 +0100

    Added GameObject class

---
+++ Core/Objects/GameObject.cpp
@@ -0,0 +1,33 @@
+//
+// Created by tangt on 26/10/2020.
+//
+// Main class that represent the common think between each objects
+//
+
+#include "GameObject.h"
+
+object::GameObject::GameObject(Math::Vector3F position, Math::Vector3F color):
+m_position(position),
+m_color(color)
+{
+}
+
+const Math::Vector3F & object::GameObject::getColor() const
+{
+    return m_color;
+}
+
+const Math::Vector3F & object::GameObject::getPosition() const
+{
+    return m_position;
+}
+
+void object::GameObject::setPosition(const Math::Vector3F &position)
+{
+    m_position = position;
+}
+
+void object::GameObject::setColor(const Math::Vector3F &color)
+{
+    m_color = color;
+}
---
+++ Core/Objects/GameObject.h
@@ -0,0 +1,38 @@
+//
+// Created by tangt on 26/10/2020.
+//
+// Main class that represent the common think between each objects
+//
+
+#ifndef GAMEENGINE_GAMEOBJECT_H
+#define GAMEENGINE_GAMEOBJECT_H
+
+#include "Vector3F.h"
+#include "Vertex.h"
+#include <vector>
+
+namespace object {
+
+    class GameObject {
+    public:
+        const Math::Vector3F & getPosition() const;
+        const Math::Vector3F & getColor() const;
+
+        void setPosition(const Math::Vector3F & position);
+        void setColor(const Math::Vector3F & color);
+
+        virtual const std::vector<graphics::Vertex> & getVertices() const = 0;
+        virtual const std::vector<uint32_t> & getIndices() const = 0;
+
+    protected:
+        GameObject(Math::Vector3F position, Math::Vector3F color);
+
+        Math::Vector3F m_position;
+        Math::Vector3F m_color;
+        std::vector<graphics::Vertex> m_vertices;
+        std::vector<uint32_t> m_indices;
+    };
+
+}
+
+#endif //GAMEENGINE_GAMEOBJECT_H

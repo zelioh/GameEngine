3e89c504dc7cd2eb1df3a1fc3df8ac041980f533
zelioh <38160465+zelioh@users.noreply.github.com>
2020-11-09 10:29:14 +0100

    Added SDK library

---
+++ SDK/Cube.cpp
@@ -0,0 +1,5 @@
+//
+// Created by FiercePC on 09/11/2020.
+//
+
+#include "Cube.h"
---
+++ SDK/Cube.h
@@ -0,0 +1,26 @@
+//
+// Created by FiercePC on 09/11/2020.
+//
+
+#ifndef GAMEENGINE_SDK_CUBE_H
+#define GAMEENGINE_SDK_CUBE_H
+
+namespace graphics
+{
+    class Cube;
+}
+
+namespace sdk {
+
+    class Cube
+    {
+    public:
+
+    private:
+        ::graphics::Cube * m_pCube;
+    };
+
+}
+
+
+#endif //GAMEENGINE_CUBE_H

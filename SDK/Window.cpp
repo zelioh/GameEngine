7338f7cdfda9a5334e15d4b7edcfa30589519adf
zelioh <38160465+zelioh@users.noreply.github.com>
2020-11-11 11:46:23 +0100

    Added Window to sdk

---
+++ SDK/Window.cpp
@@ -0,0 +1,31 @@
+//
+// Created by FiercePC on 11/11/2020.
+//
+
+#include "Window.h"
+#include "public/GraphicsEngine/Window.h"
+
+sdk::Window::Window():
+m_pWindow(graphics::Window::getInstance())
+{
+}
+
+HWND sdk::Window::getHWindow() const
+{
+    return m_pWindow->getHWindow();
+}
+
+int sdk::Window::getWidth() const
+{
+    return m_pWindow->getWidth();
+}
+
+int sdk::Window::getHeight() const
+{
+    return m_pWindow->getHeight();
+}
+
+bool sdk::Window::isFullScreen() const
+{
+    return m_pWindow->isFullScreen();
+}
